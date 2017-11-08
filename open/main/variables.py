48ff06fdf4722bb4a41c664697c809212a608185
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-08 17:49:30 +0000

    global variables to be used by application

---
+++ open/main/variables.py
@@ -0,0 +1,11 @@
+
+__all__ = ['PDF_DATA', 'CSV_DATA', 'TXT_DATA', 'JSON_DATA']
+
+PDF_DATA = '../data/pdf-sample.pdf'
+CSV_DATA = '../data/csv-sample.csv'
+TXT_DATA = '../data/text-sample.txt'
+JSON_DATA = '../data/companies.json'
+READ_ONLY = 'r'
+READ_BINARY = 'rb'
+WRITE_ONLY = 'w'
+WRITE_BINARY = 'wb'
