e6522766c33c5930ec21ea5d164287c0696f2ec3
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-09 23:52:57 +0000

    main interface the read/write main package

---
+++ open/main/__init__.py
@@ -0,0 +1,8 @@
+"""
+this is the main interface to the main package that contains several utility functions for codes to be used by
+different parts for the package and has the main export interface for the main package
+"""
+
+import magic
+import mimetypes
+
