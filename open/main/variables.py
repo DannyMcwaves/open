92d1da31eda2f9bce4760943742662bc94e90385
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-10 12:35:03 +0000

    open modes variables added

--- open/main/variables.py
+++ open/main/variables.py
@@ -1,11 +1,16 @@
 
 __all__ = ['PDF_DATA', 'CSV_DATA', 'TXT_DATA', 'JSON_DATA']
 
+# sample read write data
 PDF_DATA = '../data/pdf-sample.pdf'
 CSV_DATA = '../data/csv-sample.csv'
 TXT_DATA = '../data/text-sample.txt'
 JSON_DATA = '../data/companies.json'
+
+# open modes.
 READ_ONLY = 'r'
 READ_BINARY = 'rb'
 WRITE_ONLY = 'w'
 WRITE_BINARY = 'wb'
+READ_WRITE = 'a+'
+READ_WRITE_BINARY = 'a+b'
