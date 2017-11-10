b48b81c345f3fe2a19993d73425b66bb8aec8691
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-10 12:56:38 +0000

    pdf class

--- open/main/pdf.py
+++ open/main/pdf.py
@@ -3,4 +3,19 @@ this should a class that contains as many utility functions as possible for read
 writing data from pdf files.
 """
 
+__all__ = ['Pdf']
+
+
 import PyPDF2
+from .interface import OpenInterface
+from .variables import READ_WRITE_BINARY
+
+
+class Pdf(OpenInterface):
+
+    def __init__(self, file, action=READ_WRITE_BINARY):
+        super(Pdf, self).__init__(file, action)
+        self.pdf_file_read = PyPDF2.PdfFileReader(self.file)
+
+    def read(self):
+        """the reader for pdf"""
