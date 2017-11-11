2615448c8281426ba070b9aafb761bd8c37e8e6b
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-11 21:32:29 +0000

    pdf read and abstract methods from interface implemented

--- open/main/pdf.py
+++ open/main/pdf.py
@@ -19,3 +19,84 @@ class Pdf(OpenInterface):
 
     def read(self):
         """the reader for pdf"""
+
+    def __len__(self):
+        return self.pdf_file_read.getNumPages()
+
+    def __add__(self, other):
+        """
+        :param other:
+        :return:
+        """
+
+    def __bool__(self):
+        """
+        :return: true || false
+        """
+        return bool(len(self))
+
+    def __call__(self, *args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
+    def __contains__(self, item):
+        """
+        :param item:
+        :return:
+        """
+
+    def __enter__(self):
+        """
+        :return:
+        """
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        :param exc_type:
+        :param exc_val:
+        :param exc_tb:
+        :return:
+        """
+
+    def __getitem__(self, item):
+        """
+        :param item:
+        :return:
+        """
+        if type(item) != int or item > len(self) or item < 0:
+            return NotImplemented
+        return self.pdf_file_read.getPage(item).extractText()
+
+    def __iadd__(self, other):
+        """
+        :param other:
+        :return:
+        """
+
+    def __instancecheck__(self, instance):
+        """
+        :param instance:
+        :return:
+        """
+        return isinstance(self, instance)
+
+    def __iter__(self):
+        """
+        :return:
+        """
+        for i in range(len(self)):
+            yield self.pdf_file_read.getPage(i).extractText()
+
+    def __radd__(self, other):
+        """
+        :param other:
+        :return:
+        """
+
+    def write(self):
+        """
+        :return:
+        """
