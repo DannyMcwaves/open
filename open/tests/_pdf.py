788b16c929cbd1b8706a9177b7eb778d0114fc75
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-15 17:18:54 +0000

    refactored tests for the the pdf module

--- open/tests/_pdf.py
+++ open/tests/_pdf.py
@@ -1,18 +1,18 @@
 from open.main.pdf import Pdf
-from open.main.txt import Text
 from open.main.variables import PDF_DATA, TXT_DATA
 
 pdf = Pdf(PDF_DATA)
 pdf2 = Pdf(open(PDF_DATA, 'rb'))
-text = Text(TXT_DATA)
 
 
 print(len(pdf))
 print(bool(pdf2))
 print(isinstance(Pdf, pdf))
-print(text.read())
+print(pdf)
 
-print(pdf[-5])
+# print(pdf())
+# print(pdf.read())
+# print(pdf[-5])
 
-for i in pdf:
-    print(i)
+# for i in pdf:
+#     print(i)
