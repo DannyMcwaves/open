15f1f339da1a271962fefe0af412087eef14c430
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-17 16:19:48 +0000

    json module test file

---
+++ open/tests/__json.py
@@ -0,0 +1,10 @@
+from open.main.json import Json
+from open.main.variables import JSON_DATA
+
+json = Json(JSON_DATA)
+
+print(json)
+# print(json.json_read_file['_id'])
+# print(json.json_read_file.keys())
+# for key, value in json.json_read_file.items():
+#     print(key, value)
