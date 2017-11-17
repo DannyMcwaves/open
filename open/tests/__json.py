d987da6d8557f226e11a18ba06c2f6d5ffb4fdc1
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-17 16:26:39 +0000

    json test file

--- open/tests/__json.py
+++ open/tests/__json.py
@@ -4,7 +4,7 @@ from open.main.variables import JSON_DATA
 json = Json(JSON_DATA)
 
 print(json)
-# print(json.json_read_file['_id'])
-# print(json.json_read_file.keys())
-# for key, value in json.json_read_file.items():
-#     print(key, value)
+print(json.json_read_file['_id'])
+print(json.json_read_file.keys())
+for key, value in json.json_read_file.items():
+    print(key, value)
