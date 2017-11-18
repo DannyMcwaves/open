9c5172e2d301601e6d3df26f48fcb938fef5f4ee
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-18 18:51:51 +0000

    tests for new json procesor

--- open/tests/__json.py
+++ open/tests/__json.py
@@ -1,10 +1,19 @@
 from open.main.json import Json
-from open.main.variables import JSON_DATA
+from open.main.variables import JSON_DATA, JSON_DATA2
+import json as js
 
 json = Json(JSON_DATA)
-
 print(json)
-print(json.json_read_file['_id'])
-print(json.json_read_file.keys())
-for key, value in json.json_read_file.items():
-    print(key, value)
+print(type(json.json_read_file))
+print(len(json))
+for i in json:
+    print(i['_id'])
+
+print('\n---------------\n')
+
+json2 = Json(JSON_DATA2)
+print(json2)
+print(type(json2.json_read_file))
+print(len(json2))
+for key in json2:
+    print(key['city'])
