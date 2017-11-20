8b6a21071c6fe6ce051e25e62176ddc5a812a987
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-20 12:31:41 +0000

    data variable paths changed to absolute paths

--- open/main/variables.py
+++ open/main/variables.py
@@ -1,13 +1,18 @@
 
 __all__ = ['PDF_DATA', 'CSV_DATA', 'TXT_DATA', 'JSON_DATA', 'JSON_DATA2', 'XML_DATA']
 
+import os
+directory = os.path.dirname(__file__)
+
+
 # sample read write data
-PDF_DATA = '../data/pdf-sample.pdf'
-CSV_DATA = '../data/csv-sample.csv'
-TXT_DATA = '../data/text-sample.txt'
-JSON_DATA = '../data/companies.json'
-JSON_DATA2 = '../data/zips.json'
-XML_DATA = '../data/volume.xml'
+PDF_DATA = os.path.abspath(os.path.join(directory, '../data/pdf-sample.pdf'))
+CSV_DATA = os.path.abspath(os.path.join(directory, '../data/csv-sample.csv'))
+TXT_DATA = os.path.abspath(os.path.join(directory, '../data/text-sample.txt'))
+JSON_DATA = os.path.abspath(os.path.join(directory, '../data/companies.json'))
+JSON_DATA2 = os.path.abspath(os.path.join(directory, '../data/zips.json'))
+XML_DATA = os.path.abspath(os.path.join(directory, '../data/volume.xml'))
+
 
 # open modes.
 READ_ONLY = 'r'
