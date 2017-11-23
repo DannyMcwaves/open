306e3443c6ea7d6313c862c0c47cf82a3160963c
dannymcwaves <dannymcwaves96@gmail.com>
2017-11-23 14:28:48 +0000

    package setup file updated

--- setup.py
+++ setup.py
@@ -2,4 +2,14 @@
 this is the setup tools to support the package installation on remote machines
 """
 
-import setuptools
+from setuptools import setup
+
+setup(
+    name='open',
+    version='0.0.1',
+    package='open',
+    description='File opener for several file types',
+    author='Danny Mcwaves',
+    url='https://github.com/DannyMcwaves/open'
+)
+
